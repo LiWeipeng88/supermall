582310aba91c9b6b14caaeb5eed677f460a28dde
Your Name <you@example.com>
2019-09-24 14:21:45 +0800

    'xiangqingye'

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,27 @@ export function getDetail(iid) {
 		}
 	})
 }
+// 商品信息
+export class Goods {
+	constructor(itemInfo, columns, services) {
+	    this.title = itemInfo.title
+		this.desc = itemInfo.desc
+		this.newPrice = itemInfo.price
+		this.oldPrice = itemInfo.oldPrice
+		this.disconut = itemInfo.discountDesc
+		this.columns = columns
+		this.services = services
+		this.realPrice = itemInfo.lowNowPrice
+	}
+}
+// 商家信息
+export class Shop {
+	constructor(shopInfo) {
+	    this.logo = shopInfo.shopLogo
+		this.name = shopInfo.name
+		this.fans = shopInfo.cFans
+		this.sells = shopInfo.cSells
+		this.score = shopInfo.score
+		this.goodsCount = shopInfo.cGoods
+	}
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,24 +1,46 @@
 <template>
-	<div class="detail">
-		<DetailNavBar></DetailNavBar>
-		<DetailSwiper :topImages="topImages"></DetailSwiper>
+	<div id="detail">
+		<DetailNavBar class="detail-nav"></DetailNavBar>
+		<Scroll class="content">
+			<DetailSwiper :topImages="topImages"></DetailSwiper>
+			<DetailBaseInfo :goods="goods"></DetailBaseInfo>
+			<DetailShopInfo :shop="shop"></DetailShopInfo>
+			<DetailImageInfo :detailInfo="detailInfo"></DetailImageInfo>
+			<DetailParamsInfo :paramInfo="itemParams"></DetailParamsInfo>
+		</Scroll>
 	</div>
 </template>
 
 <script>
 	import DetailNavBar from './childComps/DetailNavBar'
 	import DetailSwiper from './childComps/DetailSwiper'
-	import {getDetail} from 'network/detail.js'
+	import DetailBaseInfo from './childComps/DetailBaseInfo'
+	import DetailShopInfo from './childComps/DetailShopInfo'
+	import DetailImageInfo from './childComps/DetailImageInfo'
+	import DetailParamsInfo from './childComps/DetailParamsInfo'
+	
+	import Scroll from "components/common/scroll/Scroll";
+	
+	import {getDetail, Goods, Shop } from 'network/detail.js'
 	export default{
 		name:'Detail',
 		components: {
 			DetailNavBar,
-			DetailSwiper
+			DetailSwiper,
+			DetailBaseInfo,
+			DetailShopInfo,
+			DetailImageInfo,
+			DetailParamsInfo,
+			Scroll
 		},
 		data() {
 			return {
 				iid: null,
-				topImages:[]
+				topImages:[],
+				goods:{},
+				shop:{},
+				detailInfo:{},
+				itemParams:{}
 			}
 		},
 		created(){
@@ -27,12 +49,35 @@
 			// 2.根据id请求详情数据
 			getDetail(this.iid).then(res => {
 				console.log(res)
-				// 获取顶部轮播图数据
-				this.topImages = res.result.itemInfo.topImages
+				// 1.获取顶部轮播图数据
+				const data = res.result
+				this.topImages = data.itemInfo.topImages
+				// 2.获取商品信息
+				this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+				// 创建店铺信息对象
+				this.shop = new Shop(data.shopInfo)
+				// 商品的详情数据
+				this.detailInfo = data.detailInfo
+				// 取出参数信息
+				this.itemParams = data.itemParams
 			})
 		}
 	}
 </script>
 
 <style scoped="scoped">
+	#detail{
+		position: relative;
+		z-index: 9;
+		background-color: #FFFFFF;
+		height: 100vh;
+	}
+	.detail-nav{
+		position: relative;
+		z-index: 9;
+		background-color: #FFFFFF;
+	}
+	.content {
+		height: calc(100% - 44px);
+	}
 </style>
